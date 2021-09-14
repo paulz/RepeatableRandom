2e0bdd0ffd248ff9542a7c7ecd71cdb3cdf3c03c
Paul Zabelin <paulz@users.noreply.github.com>
2021-09-13 22:30:58 -0700

    add macOS test target, remove app target from example

--- Example/Pods/Target Support Files/Pods-macOS-Tests/Pods-macOS-Tests-umbrella.h
+++ Example/Pods/Target Support Files/Pods-macOS-Tests/Pods-macOS-Tests-umbrella.h
@@ -11,6 +11,6 @@
 #endif
 
 
-FOUNDATION_EXPORT double Pods_RepeatableRandom_TestsVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_RepeatableRandom_TestsVersionString[];
+FOUNDATION_EXPORT double Pods_macOS_TestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_macOS_TestsVersionString[];
 
