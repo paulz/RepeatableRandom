fab5ba57acf01cf814ed8bcd17300ffaf049ba7e
Paul Zabelin <paulz@users.noreply.github.com>
2021-09-13 23:25:36 -0700

    verify test order

--- Example/Tests/Tests.swift
+++ Example/Tests/Tests.swift
@@ -1,8 +1,22 @@
 import XCTest
 
-class Tests: XCTestCase {
-    func testExample() {
-        XCTAssert(true, "Pass")
+class FirstTest: XCTestCase {
+    func testFirst() {
+        XCTAssertEqual(1, 1, "should pass")
+    }
+    func testSecond() {
+        XCTAssertEqual(2, 2, "should pass")
     }
 }
 
+class SecondTest: XCTestCase {
+    func testSecond() {
+        XCTAssertEqual(2, 2, "should pass")
+    }
+}
+
+class ThirdTest: XCTestCase {
+    func testThird() {
+        XCTAssertEqual(3, 3, "should pass")
+    }
+}
--- Example/Tests/VerifyTestOrderObserver.swift
+++ Example/Tests/VerifyTestOrderObserver.swift
@@ -3,6 +3,30 @@ import XCTest
 class VerifyTestOrderObserver: NSObject, XCTestObservation {
     static var shared = VerifyTestOrderObserver()
     func testSuiteDidFinish(_ testSuite: XCTestSuite) {
-        print(testSuite.tests)
+        if testSuite.name == "All tests" {
+            assert(
+                testSuite.testNames ==
+                    [
+                        "-[FirstTest testFirst]",
+                        "-[FirstTest testSecond]",
+                        "-[SecondTest testSecond]",
+                        "-[ThirdTest testThird]"
+                    ]
+            )
+        }
+    }
+}
+
+extension XCTestSuite {
+    var testNames: [String] {
+        tests.map {
+            if let suite = $0 as? XCTestSuite {
+                return suite.testNames
+            } else if let testCase = $0 as? XCTestCase {
+                return [testCase.name]
+            } else {
+                return [name]
+            }
+        }.reduce([], +)
     }
 }
